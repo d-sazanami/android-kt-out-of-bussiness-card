320b4718c87e5415e3377c4afe5ae42b9a1e55c8
sazanami <dstrsazanami@gmail.com>
2021-10-22 22:25:19 +0900

    設定値のキー名定数宣言に修正

--- app/src/main/java/com/example/sazanami/outofbusinesscards/Common.kt
+++ app/src/main/java/com/example/sazanami/outofbusinesscards/Common.kt
@@ -3,5 +3,16 @@ package com.example.sazanami.outofbusinesscards
 class Common {
     companion object {
         const val sharedPreferenceName: String = "BusinessCard"
+
+        const val prefCompany = "company"
+        const val prefPostal = "postal"
+        const val prefAddress = "address"
+        const val prefTel = "Tel"
+        const val prefFax = "Fax"
+        const val prefEmail = "Email"
+        const val prefUrl = "Url"
+        const val prefPosition = "Position"
+        const val prefName = "Name"
+
     }
 }
